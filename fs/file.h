b5c805bacf7362085dccd60c3e99e0bd262eb62f
Zaire <3147879462@qq.com>
2023-12-22 14:34:36 +0800

    feat sys_close

--- fs/file.h
+++ fs/file.h
@@ -32,4 +32,6 @@ int32_t block_bitmap_alloc(struct partition *part);
 void bitmap_sync(struct partition *part, uint32_t bit_idx, uint8_t btmp_type);
 int32_t file_create(struct dir *parent_dir, char *filename, uint8_t flag);
 extern struct file file_table[MAX_FILE_OPEN];
+int32_t file_open(uint32_t inode_no, uint8_t flag);
+int32_t file_close(struct file *file);
 #endif
--- kernel/main.c
+++ kernel/main.c
@@ -1,11 +1,15 @@
 #include "fs.h"
 #include "init.h"
 #include "interrupt.h"
+#include "stdio.h"
 
 int main(void) {
     init_all();
     intr_enable();
-    sys_open("/file1", O_CREAT);
+    uint32_t fd = sys_open("/file1", O_RDONLY);
+    printf("fd:%d\n", fd);
+    sys_close(fd);
+    printf("%d closed now\n", fd);
     while (1) {
     };
     return 0;
