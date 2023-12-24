742f111ab017677f89293cc1bba6c39767390827
Zaire <3147879462@qq.com>
2023-12-24 10:25:20 +0800

    feat sys_unlink

--- kernel/main.c
+++ kernel/main.c
@@ -7,27 +7,7 @@
 int main(void) {
     init_all();
     intr_enable();
-    uint32_t fd = sys_open("/file1", O_RDWR);
-    printf("open /file1, fd:%d\n", fd);
-    char buf[64] = {0};
-    int read_bytes = sys_read(fd, buf, 18);
-    printf("1_ read %d bytes:\n%s\n", read_bytes, buf);
-
-    memset(buf, 0, 64);
-    read_bytes = sys_read(fd, buf, 6);
-    printf("2_ read %d bytes:\n%s", read_bytes, buf);
-
-    memset(buf, 0, 64);
-    read_bytes = sys_read(fd, buf, 6);
-    printf("3_ read %d bytes:\n%s", read_bytes, buf);
-
-    printf("________  SEEK SET 0  ________\n");
-    sys_lseek(fd, 0, SEEK_SET);
-    memset(buf, 0, 64);
-    read_bytes = sys_read(fd, buf, 24);
-    printf("4_ read %d bytes:\n%s", read_bytes, buf);
-
-    sys_close(fd);
+    printf("/file1 delete %s!\n", sys_unlink("/file1") == 0 ? "done" : "fail");
     while (1) {
     };
     return 0;
