1ce8ee673acea48461b8c786f6cc211cdb5486ae
Zaire <3147879462@qq.com>
2023-12-24 11:04:35 +0800

    feat sys_readdir and sys_rewinddir

--- kernel/main.c
+++ kernel/main.c
@@ -1,3 +1,4 @@
+#include "dir.h"
 #include "fs.h"
 #include "init.h"
 #include "interrupt.h"
@@ -9,7 +10,17 @@ int main(void) {
     intr_enable();
     struct dir *p_dir = sys_opendir("/dir1/subdir1");
     if (p_dir) {
-        printf("/dir1/subdir1 open done!\n");
+        printf("/dir1/subdir1 open done!\ncontent:\n");
+        char *type = NULL;
+        struct dir_entry *dir_e = NULL;
+        while ((dir_e = sys_readdir(p_dir))) {
+            if (dir_e->f_type == FT_REGULAR) {
+                type = "regular";
+            } else {
+                type = "directory";
+            }
+            printf("      %s   %s\n", type, dir_e->filename);
+        }
         if (sys_closedir(p_dir) == 0) {
             printf("/dir1/subdir1 close done!\n");
         } else {
