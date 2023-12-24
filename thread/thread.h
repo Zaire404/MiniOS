6b102fc616297b923893ccc3bdd914e3de21c6d0
Zaire <3147879462@qq.com>
2023-12-24 13:14:10 +0800

    feat sys_getcwd and sys_chdir

--- thread/thread.h
+++ thread/thread.h
@@ -88,8 +88,9 @@ struct task_struct {
 
     uint32_t* pgdir;                               // 进程自己页表的虚拟地址
     struct virtual_addr userprog_vaddr;            // 用户进程的虚拟地址
-    uint32_t stack_magic;                          // 魔数, 栈的边界标记, 用于检测栈的溢出
     struct mem_block_desc u_block_desc[DESC_CNT];  // 用户进程内存块描述符
+    uint32_t cwd_inode_nr;                         // 进程所在的工作目录的inode编号
+    uint32_t stack_magic;                          // 魔数, 栈的边界标记, 用于检测栈的溢出
 };
 
 extern struct list thread_ready_list;
